014e9d9a7124abf8e957b67f62c9b6ffe8d2d530
Phong <phongpham.dev@gmail.com>
2024-07-20 22:11:48 +0700

    first commit

---
+++ base-security/src/main/java/basesecurity/BaseSecurityApplication.java
@@ -0,0 +1,13 @@
+package basesecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BaseSecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BaseSecurityApplication.class, args);
+	}
+
+}
